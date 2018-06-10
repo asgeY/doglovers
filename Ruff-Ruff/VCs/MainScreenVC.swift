f439a6d2520edbb691fc9601bdb7debd326e427b
Ruff-Ruff <tryruffruff@gmail.com>
2018-06-10 02:16:28 -0400

    Something

--- Ruff-Ruff/VCs/MainScreenVC.swift
+++ Ruff-Ruff/VCs/MainScreenVC.swift
@@ -10,11 +10,14 @@ import UIKit
 import Firebase
 import KeychainSwift
 
-class MainScreenVC: UIViewController {
+class MainScreenVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        collectionView.delegate = self
+        collectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -23,6 +26,23 @@ class MainScreenVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return 8
+    }
+    
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath )
+        
+        //cell.imageCell
+        return cell
+    }
+    
+
 
     @IBAction func logoutClicked(_ sender: Any) {
         let firebaseAuth = FIRAuth.auth()
@@ -35,6 +55,8 @@ class MainScreenVC: UIViewController {
         dismiss(animated: true, completion: nil)
         
     }
+    
+    
     
 
 }
